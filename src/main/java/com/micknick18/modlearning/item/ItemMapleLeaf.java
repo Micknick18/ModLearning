0b84c967f5fa10c89bce8fabea9d949933fe6741
Micknick18 <alexander.ducker@hotmail.co.uk>
2014-07-29 00:44:06 +0100

    First Item

---
+++ src/main/java/com/micknick18/modlearning/item/ItemMapleLeaf.java
@@ -0,0 +1,10 @@
+package com.micknick18.modlearning.item;
+
+public class ItemMapleLeaf extends ItemML
+{
+    public ItemMapleLeaf()
+    {
+        super();
+        this.setUnlocalizedName("mapleLeaf");
+    }
+}
